fbd283f9a0634949dcec5b123b697880a772b54a
khw <khj930410@naver.com>
2022-06-24 17:22:17 +0900

    Leveraging React Context

--- formik_tutorial/src/App.js
+++ formik_tutorial/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import {useFormik} from 'formik'
+import {Formik} from 'formik';
+// import {useFormik} from 'formik'
 import * as Yup from 'yup';
 
 
@@ -28,13 +29,19 @@ import * as Yup from 'yup';
 
 function App() {
 
-  const formik = useFormik({
-    initialValues:{
+
+  
+  return (
+    <>
+
+
+<Formik
+    initialValues={{
       firstName:'',
       lastName:'',
       email:''
-    },
-    validationSchema: Yup.object({
+    }}
+    validationSchema={Yup.object({
       firstName: Yup.string()
       .max(15, 'Must be 15 characters or less')
       .required('Required'),
@@ -42,55 +49,53 @@ function App() {
       .max(20, 'Must be 20 character or less')
       .required('required'),
       email:Yup.string().email('Invalid email address').required('Required'),
-    }),
-    onSubmit:values=>{
-      
-      // alert(values);
-      alert(JSON.stringify(values,null,2))
-    },
-  });
-  
-  return (
-    <>
-      <form onSubmit={formik.handleSubmit}>
-        <label htmlFor="firstName">firstName</label>
-        <input
-          id="firstName"
-          name='firstName'
-          {...formik.getFieldProps('firstName')}
-          // type="text"
-          // onChange={formik.handleChange}
-          // onBlur={formik.handleBlur}
-          // value={formik.values.firstName}
-        />
-       {formik.touched.firstName && formik.errors.firstName? (<div>{formik.errors.firstName}</div>): null}
+    })}
+    onSubmit={
+      (values,{setSubmitting})=>{
+        setTimeout(()=>{
+          alert(JSON.stringify(values,null,2));
+          setSubmitting(false)
+        },400)
+      }
+    }
+  >
+    {formik=>(
+            <form onSubmit={formik.handleSubmit}>
+            <label htmlFor="firstName">firstName</label>
+            <input
+              id="firstName"
+              type="text"
+              
+              {...formik.getFieldProps('firstName')}
 
-        <label htmlFor='lastName'>lastName</label>
-        <input
-          id="lastName"
-          name='lastName'
-          {...formik.getFieldProps('lastName')}
-          // type="text"
-          // onChange={formik.handleChange}
-          // onBlur={formik.handleBlur}
-          // value={formik.values.lastName}
-        />
-        {formik.touched.lastName && formik.errors.lastName? <div>{formik.errors.lastName}</div>:null} 
+            />
+           {formik.touched.firstName && formik.errors.firstName? (<div>{formik.errors.firstName}</div>): null}
+    
+            <label htmlFor='lastName'>lastName</label>
+            <input
+              id="lastName"
+              type="text"
+              {...formik.getFieldProps('lastName')}
+              // type="text"
+              // onChange={formik.handleChange}
+              // onBlur={formik.handleBlur}
+              // value={formik.values.lastName}
+            />
+            {formik.touched.lastName && formik.errors.lastName? <div>{formik.errors.lastName}</div>:null} 
+    
+          
+            <label htmlFor='email'>Email Address</label>
+            <input
+              id="email"
+              type="email"
+              {...formik.getFieldProps('email')}
+            />
+            {formik.touched.email &&formik.errors.email?<div>{formik.errors.email}</div>:null}
+            <button type="submit">Submit</button>
+          </form> 
+    )}
+  </Formik>
 
-      
-        <label htmlFor='email'>Email Address</label>
-        <input
-          id="email"
-          name="email"
-          {...formik.getFieldProps('email')}
-          // type="email"
-          // onChange={formik.handleChange}
-          // onBlur={formik.handleBlur}
-          // value={formik.values.email}
-        />
-        {formik.touched.email &&formik.errors.email?<div>{formik.errors.email}</div>:null}
-        <button type="submit">Submit</button>
-      </form> 
     </>
   );
 }
