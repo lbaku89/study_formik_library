5c79112d48e6892dbe56da8a5de364472ed6debf
khw <khj930410@naver.com>
2022-06-24 12:38:12 +0900

    Schema Validation with Yup

--- formik_tutorial/src/App.js
+++ formik_tutorial/src/App.js
@@ -1,30 +1,30 @@
 import './App.css';
 import {useFormik} from 'formik'
-// import {useState} from 'react'
+import * as Yup from 'yup';
 
 
-const validate = values =>{
-  const errors = {};
-  if(!values.firstName){
-    errors.firstName='Required';
-  }else if(values.firstName.length>15){
-    errors.firstName = 'Must be 15 characters or less';
-  }
+// const validate = values =>{
+//   const errors = {};
+//   if(!values.firstName){
+//     errors.firstName='Required';
+//   }else if(values.firstName.length>15){
+//     errors.firstName = 'Must be 15 characters or less';
+//   }
 
-  if(!values.lastName){
-    errors.lastName = 'Required';
-  }else if(values.lastName.length>20){
-    errors.lastName = "Must be 20 character or less";
-  }
+//   if(!values.lastName){
+//     errors.lastName = 'Required';
+//   }else if(values.lastName.length>20){
+//     errors.lastName = "Must be 20 character or less";
+//   }
 
-  if(!values.email){
-    errors.email = 'Required';
-  }else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)){
-    errors.email = 'Invalid email address'     
-  }
+//   if(!values.email){
+//     errors.email = 'Required';
+//   }else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)){
+//     errors.email = 'Invalid email address'     
+//   }
 
-  return errors;
-};
+//   return errors;
+// };
 
 function App() {
   
@@ -34,7 +34,15 @@ function App() {
       lastName:'',
       email:''
     },
-    validate,
+    validationSchema: Yup.object({
+      firstName: Yup.string()
+      .max(15, 'Must be 15 characters or less')
+      .required('Required'),
+      lastName: Yup.string()
+      .max(20, 'Must be 20 character or less')
+      .required('required'),
+      email:Yup.string().email('Invalid email address').required('Required'),
+    }),
     onSubmit:values=>{
       
       // alert(values);
